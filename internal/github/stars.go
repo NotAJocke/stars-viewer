106f5188c8016e7bccf611afe8dda583d6a85d45
NotAJocke <jockerider199@gmail.com>
2024-05-20 16:27:04 +0200

    add unstar repo feature

--- internal/github/stars.go
+++ internal/github/stars.go
@@ -72,3 +72,23 @@ func GetStarredRepos() []StarredRepo {
 
 	return repos
 }
+
+func UnstarRepo(fullName string) {
+	url := fmt.Sprintf("https://api.github.com/user/starred/%s", fullName)
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		log.Fatalln("Couldn't create request to github.")
+	}
+
+	token := os.Getenv("GH_TOKEN")
+	auth := fmt.Sprintf("Bearer %s", token)
+	req.Header.Set("Authorization", auth)
+	req.Header.Set("Accept", "application/vnd.github+json")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalln("Error making request:", err)
+	}
+	defer res.Body.Close()
+}
